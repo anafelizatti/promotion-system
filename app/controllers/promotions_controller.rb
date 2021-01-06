f069c06ba3a806ee7bf248b36363e6c98a20d5e5
anafelizatti <apfelizatti@gmail.com>
2021-01-06 17:44:14 -0300

    Admin registra promoção

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -7,4 +7,20 @@ class PromotionsController < ApplicationController
     @promotion = Promotion.find(params[:id])
   end
 
+  def new
+    @promotion = Promotion.new
+  end
+
+  def create
+    @promotion = Promotion.create(promotion_params)
+    redirect_to @promotion
+  end
+
+  private 
+    def promotion_params
+      params
+    .require(:promotion)
+    .permit(:name, :description, :code, :discount_rate, :expiration_date, :coupon_quantity)
+    end
+
 end
