6568c4dd6fa81a53b045cefbf1a79a8f02e9546f
anafelizatti <apfelizatti@gmail.com>
2021-01-07 17:44:20 -0300

    WIP Traduções e Autenticação

---
+++ spec/features/user_log_in_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+feature 'User log in' do 
+
+    scenario 'and receive welcome message' do
+        User.create!(email: 'jane_doe@locaweb.com.br', password: '123456')
+
+        visit root_path
+        click_on 'Login'
+        fill_in 'Email', with: 'jane_doe@locaweb.com.br'
+        fill_in 'Senha', with: '123456'
+        click_on 'Entrar'
+
+        expect(current_path).to eq(root_path)
+        expect(page).to have_content('Login efetuado com sucesso')
+        expect(page).to have_content('jane_doe@locaweb.com.br')
+        expect(page).to_not have_link('Login')
+        expect(page).to have_link('Sair')
+
+    end
+    
+    xscenario 'and log out' do
+        User.create!(email: 'jane_doe@locaweb.com.br', password: '123456')
+
+        visit root_path
+        click_on 'Login'
+        fill_in 'Email', with: 'jane_doe@locaweb.com.br'
+        fill_in 'Senha', with: '123456'
+        click_on 'Entrar'
+        click_on 'Sair'
+
+        expect(current_path).to eq(root_path)
+        expect(page).to_not have_content('Login efetuado com sucesso')
+        expect(page).to_not have_content('jane_doe@locaweb.com.br')
+        expect(page).to have_link('Login')
+        expect(page).to_not have_link('Sair')
+
+    end
+
+    
+end
