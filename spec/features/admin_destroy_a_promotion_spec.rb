16ec37a5c2d76573639f89df67cea89804301a40
anafelizatti <apfelizatti@gmail.com>
2021-01-08 16:59:35 -0300

    Admin destroy a promotion

--- spec/features/admin_destroy_a_promotion_spec.rb
+++ spec/features/admin_destroy_a_promotion_spec.rb
@@ -2,16 +2,15 @@ require 'rails_helper'
 
 feature 'Admin destroys a promotion' do
   
-  xscenario 'sucessfully' do
+  scenario 'sucessfully' do
     promotion = Promotion.create!(name: 'Dia do consumidor', description: 'Promoção dia dos consumidores',
                       code: 'CONS10', discount_rate: 10, coupon_quantity: 100,
                       expiration_date: '22/12/2033')
+
     visit promotions_path
     click_on promotion.name
     click_on 'Deletar'
-        #message = accept_prompt(with: 'Item será removido. Confirmar ação') do
-            #click_link('OK')
-        #end
+    
     visit promotions_path
     expect(page).to_not have_content(promotion)
     expect(current_path).to eq promotions_path
