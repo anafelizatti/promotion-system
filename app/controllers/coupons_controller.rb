6dac8a05f698f691efddee7e215c160ac72f120b
anafelizatti <apfelizatti@gmail.com>
2021-01-13 17:36:23 -0300

    WIP Status Inativo - Cupom

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,10 @@
+class CouponsController <  ApplicationController
+
+    def inactivate
+        @coupon = Coupon.find(params[:id])
+
+        flash[:success] = 'Cupom cancelado com sucesso'
+        redirect_to @coupon.promotion
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   root'home#index'
+  
   resources :promotions, only: %i[index show new create update edit destroy] do
     post 'generate_coupons', on: :member
   end
+
   resources :product_categories, only: %i[index show new create destroy edit update]
+
+  resources :coupons, only: [] do
+    post 'inactivate', on: :member
+  end
+
 end
