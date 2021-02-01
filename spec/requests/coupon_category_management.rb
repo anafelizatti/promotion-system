17765cdde03a572a7ae13b7523416b68d6431abe
anafelizatti <apfelizatti@gmail.com>
2021-02-01 11:43:01 -0300

    coupon can be burned when category and promotion category is present-API

--- spec/requests/coupon_category_management.rb
+++ spec/requests/coupon_category_management.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe 'Coupon category management' do
 
     context 'validates category'
-        it 'coupon cannot be burned unless category is checked' do
+        it 'coupon cannot be burned unless category is present' do
             promotion = create(:promotion)
             product_category = create (:product_category)
 
@@ -19,6 +19,19 @@ describe 'Coupon category management' do
 
         end
 
+        it 'coupon can be burned when category and promotion category is present' do
+            promotion = create(:promotion)
+            coupon = Coupon.create!( promotion :promotion, code: 'CONS10', status: :active)
+            product_category = create (:product_category)
+
+            promotion.product_category_ids = [product_category.id]
+            promotion.save!
+
+            patch "/api/v1/coupons/#{coupon.code}/burn", params: { order: { code: 'ORDER123'}, category: "HOSP" }
+
+            expect(response).to have_http_status(200)
+            expect(response.body).to include('Cupom utilizado com sucesso')
+        end
     end
 
 end
