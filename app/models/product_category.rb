3c7523255c4da53fa75ca6a7687e33cf70fd748e
anafelizatti <apfelizatti@gmail.com>
2021-02-02 10:04:38 -0300

    TDD ProductCategory Model Validations

--- app/models/product_category.rb
+++ app/models/product_category.rb
@@ -2,7 +2,7 @@ class ProductCategory < ApplicationRecord
     has_many :product_category_promotions, dependent: :destroy
     has_many :promotions
     validates :name, :code, presence: true
-    validates :code, uniqueness: { message: 'deve ser único'}
+    validates :name, :code, uniqueness: true
     scope :search, ->(query) {where('name like ? OR code like ?', "%#{query}%", "%#{query}%")}
     enum status: {allow: 0, disallow: 10}
 
--- spec/models/product_category_spec.rb
+++ spec/models/product_category_spec.rb
@@ -1,4 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe ProductCategory do
+  context 'validation' do
+    it 'attributes cannot be blank' do
+      product_category = ProductCategory.new
+      product_category.save
+      product_category.valid?
+
+      expect(product_category.errors[:name]).to include('não pode ficar em branco')
+      expect(product_category.errors[:code]).to include('não pode ficar em branco')
+    end
+
+    it 'attributes must be uniq' do
+      ProductCategory.create!(name: 'Hospedagem', code: 'HOSP')
+      product_category = ProductCategory.new(name: 'Hospedagem', code: 'HOSP')
+      product_category.valid?
+
+      expect(product_category.errors[:name]).to include('já está em uso')
+      expect(product_category.errors[:code]).to include('já está em uso')
+    end
+  end
 end
