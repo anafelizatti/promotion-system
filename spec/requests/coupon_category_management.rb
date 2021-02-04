73438655802be257651710109c275d3797de80cb
anafelizatti <apfelizatti@gmail.com>
2021-02-04 10:50:53 -0300

    Ofensas do Rucopo ok

--- spec/requests/coupon_category_management.rb
+++ spec/requests/coupon_category_management.rb
@@ -1,37 +1,34 @@
 require 'rails_helper'
 
 describe 'Coupon' do
+  context 'when validates category' do
+    it 'coupon cannot be burned unless category is present' do
+      promotion = create(:promotion)
+      product_category = create(:product_category)
 
-    context ' when validates category' do
-        it 'coupon cannot be burned unless category is present' do
-            promotion = create(:promotion)
-            product_category = create (:product_category)
+      promotion.product_category_ids = [product_category.id]
+      promotion.save!
 
-            promotion.product_category_ids = [product_category.id]
-            promotion.save!
+      coupon = Coupon.create!(promotion: promotion, code: 'CONS10', status: :active)
 
-            coupon = Coupon.create!( promotion :promotion, code: 'CONS10', status: :active)
+      patch "/api/v1/coupons/#{coupon.code}/burn", params: { order: { code: 'ORDER123' } }
 
-            patch "/api/v1/coupons/#{coupon.code}/burn", params: { order: { code: 'ORDER123'} }
-
-            expect(response).to have_http_status(401)
-            expect(response.body).to include('Categoria não encontrada. Ação não pode ser realizada')
-
-        end
-
-        it 'coupon can be burned when category and promotion category is present' do
-            promotion = create(:promotion)
-            coupon = Coupon.create!( promotion :promotion, code: 'CONS10', status: :active)
-            product_category = create (:product_category)
-
-            promotion.product_category_ids = [product_category.id]
-            promotion.save!
-
-            patch "/api/v1/coupons/#{coupon.code}/burn", params: { order: { code: 'ORDER123'}, category: "HOSP" }
-
-            expect(response).to have_http_status(200)
-            expect(response.body).to include('Cupom utilizado com sucesso')
-        end
+      expect(response).to have_http_status(:unauthorized)
+      expect(response.body).to include('Categoria não encontrada. Ação não pode ser realizada')
     end
 
+    it 'coupon can be burned when category and promotion category is present' do
+      promotion = create(:promotion)
+      coupon = Coupon.create!(promotion: promotion, code: 'CONS10', status: :active)
+      product_category = create(:product_category)
+
+      promotion.product_category_ids = [product_category.id]
+      promotion.save!
+
+      patch "/api/v1/coupons/#{coupon.code}/burn", params: { order: { code: 'ORDER123' }, category: 'HOSP' }
+
+      expect(response).to have_http_status(:ok)
+      expect(response.body).to include('Cupom utilizado com sucesso')
+    end
+  end
 end
